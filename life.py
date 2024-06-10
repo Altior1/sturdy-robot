920b70d96e181891d38e5de58ecda9fc4fb47520
Altior1 <thomas.rud@outlook.fr>
2024-06-10 08:51:23 +0200

    fonction jeu de la vie opérationnel

--- life.py
+++ life.py
@@ -2,7 +2,7 @@ import pyxel as px
 WIDTH = 800
 HEIGHT = 800
 TAILLE_PIX = 5
-
+INITCEL= [(1,2),(5,3),(0,0),(20,20),(80,80),(80,81),(18,159)]
 class cellule:
     """ les cellules vont être la base du changement de comportement"""
     def __init__(self,x,y):
@@ -23,7 +23,7 @@ class cellule:
             self.etat=1
     def draw(self):
         if self.etat==1:
-            px.rect(self.x*5,self.y*5,5,5,12)
+            px.rect(self.x*5,self.y*5,5,5,5)
         else:
             px.rect(self.x*5,self.y*5,5,5,15)
 
@@ -31,14 +31,13 @@ class App:
     """ La classe App contient les fonctions générales de l'application, qui vont appeler les 
     différentes classes"""
     def __init__(self):
-        px.init(WIDTH,HEIGHT,title="jeu de la vie")
-        color=px.colors.from_list([0x111111,0x222222,0x333333])
+        px.init(WIDTH,HEIGHT,title="jeu de la vie",fps=5)
         # self.list contient des listes de celulles correspondant aux différentes colonnes
         self.list=[]
-        for i in range(80):
+        for i in range(160):
             #Le i est mis sur les lignes
             list_cel=[]
-            for j in range(80):
+            for j in range(160):
                 #le j est mis sur les colonnes1
                 cel=cellule(i,j)
                 list_cel.append(cel)
@@ -50,10 +49,13 @@ class App:
                     self.list[i][j].voisin.append(self.list[i-1][j])
                 if j!=0:
                     self.list[i][j].voisin.append(self.list[i][j-1])
-                if i!=79:
+                if i!=159:
                     self.list[i][j].voisin.append(self.list[i+1][j])
-                if j!=79:
-                    self.list[i][j].voisin.append(self.list[i][j+1])    
+                if j!=159:
+                    self.list[i][j].voisin.append(self.list[i][j+1])
+        for each in INITCEL:
+            self.list[each[0]][each[1]].etat=1
+
         px.run(self.update,self.draw)
 
     def update(self):
@@ -63,7 +65,6 @@ class App:
         if px.btnp(px.KEY_Q):
             px.quit()
     def draw(self):
-        px.cls(15)
         for each in self.list:
             for ea in each:
                 ea.draw()
